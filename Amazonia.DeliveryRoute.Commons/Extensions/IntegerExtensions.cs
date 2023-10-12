0642e056d05ade22086626c711052a9300ee7a21
José Ricardo Prado de Almeida <zehpavora@gmail.com>
2023-10-12 16:48:19 -0300

    Adding extension to convert int to excel style numbering

---
+++ Amazonia.DeliveryRoute.Commons/Extensions/IntegerExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace Amazonia.DeliveryRoute.Commons.Extensions;
+
+public static class IntegerExtensions
+{
+    #region Constants
+    /// <summary>
+    /// Number of letters, from A - Z
+    /// </summary>
+    public const int AlphabeticCount = 26;
+    #endregion
+
+    /// <summary>
+    /// Converts a number to a alpha-representation
+    /// where A = 0, B = 1, etc.
+    /// </summary>
+    /// <param name="value">Value to convert from</param>
+    /// <returns>Alpha index of the value</returns>
+    public static string AsColumnName(this int value)
+    {
+        var columnName = string.Empty;
+
+        // Copies value in order not to change the parameter
+        // avoids unexpected behavior
+        var index = value;
+
+        while (index > 0)
+        {
+            var modulo = (index - 1) % AlphabeticCount;
+
+            // 'A' is the first letter so count must start from there
+            columnName = Convert.ToChar('A' + modulo) + columnName;
+            index = (index - modulo) / AlphabeticCount;
+        }
+
+        return columnName;
+    }
+}
