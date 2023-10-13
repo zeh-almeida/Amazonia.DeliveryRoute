5399ab38628af32034c72ccd27a5d973a40413d4
José Ricardo Prado de Almeida <zehpavora@gmail.com>
2023-10-12 21:38:09 -0300

    Moving unit test projects to the Unit.Test namespace Fixing directory packages

---
+++ Amazonia.DeliveryRoute.Api/Models/Position.cs
@@ -0,0 +1,43 @@
+﻿using Amazonia.DeliveryRoute.Commons.Extensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace Amazonia.DeliveryRoute.Api.Models;
+
+/// <summary>
+/// Definition of a Position in the Delivery board for Amazonia
+/// </summary>
+public sealed record Position
+{
+    #region Constants
+    /// <summary>
+    /// Maximum value for <see cref="Position.Y"/>
+    /// </summary>
+    public const int MaxY = 7;
+
+    /// <summary>
+    /// Minimum value for <see cref="Position.Y"/>
+    /// </summary>
+    public const int MinY = 0;
+    #endregion
+
+    #region Properties
+    /// <summary>
+    /// Alpha string which indicates the position in the X-axis.
+    /// Must be a single character from A-Z
+    /// <see cref="StringExtensions.ValidationPattern"/>
+    /// </summary>
+    [Required]
+    [StringLength(1, MinimumLength = 1)]
+    [RegularExpression(StringExtensions.ValidationPattern)]
+    public required string X { get; set; }
+
+    /// <summary>
+    /// Position in the Y-axis
+    /// <see cref="MaxY"/>
+    /// <see cref="MinY"/>
+    /// </summary>
+    [Required]
+    [Range(MinY, MaxY)]
+    public required int Y { get; set; }
+    #endregion
+}
---
+++ Test.Unit.Amazonia.DeliveryRoute.Api/Models/PositionTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Unit.Amazonia.DeliveryRoute.Commons.Models;
+
+public sealed record PositionTest
+{
+
+}
